d0b3e8259baf1f9bd44aac416a251a420c6c7e50
Stephen <stephensouness@outlook.com>
2025-03-14 15:30:44 +0000

    fixed the jwt secret in .env

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 const { body, validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
 
-const JWT_SECRET = process.env.JWT_SECRET || "default_secret";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const alphaErr = "must only contain letters";
 const lengthErr = "must be between 8 and 20 characters";
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const JWT_SECRET = process.env.JWT_SECRET || "default_secret";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const auth = (req, res, next) => {
   const authHeader = req.header("Authorization");
